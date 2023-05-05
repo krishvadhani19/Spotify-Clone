497477169596e1a3892582736fb11bf88c7b0965
krishvadhani19 <krish.vadhani@somaiya.edu>
2023-05-05 16:24:15 +0530

    tailwind setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-
-  return <></>;
+  return (
+    <>
+      <div className="text-red-500 text-3xl">Hello</div>
+    </>
+  );
 }
 
 export default App;
