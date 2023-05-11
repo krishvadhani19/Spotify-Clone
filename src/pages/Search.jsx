e6c379e8ad8832aaae09f505a9040c911ef98ff6
krishvadhani19 <krish.vadhani@somaiya.edu>
2023-05-11 19:41:34 +0530

    chnages added to search page as well

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,6 +3,7 @@ import Heading from "../components/Home/Heading";
 import MostLikely from "../components/Home/MostLikely";
 import TopMixes from "../components/Home/TopMixes";
 import Player from "../components/Player";
+import Controls from "../components/Home/Controls";
 
 const Search = () => {
   return (
@@ -14,35 +15,47 @@ const Search = () => {
         </div>
 
         {/* Main Page */}
-        <div className="ml-[21%] flex flex-col space-y-4 w-[80%] h-full bg-dark-200 p-4 rounded-md overflow-y-auto">
-          {/* Heading */}
-          <div className="w-full">
-            <Heading />
+        <div className="ml-[21%] flex flex-col space-y-4 w-[80%] h-full bg-dark-200 rounded-md overflow-y-auto">
+          <div className="flex flex-col space-y-4 w-full bg-gradient-to-b from-cyan-800 to-dark-200 pt-4 px-4">
+            {/* controls */}
+            <div className="w-full">
+              <Controls />
+            </div>
+
+            {/* sponsored */}
+            <div className="w-full"></div>
+
+            {/* Heading */}
+            <div className="w-full">
+              <Heading />
+            </div>
+
+            {/* Most Liked */}
+            <div className="w-full">
+              <MostLikely />
+            </div>
+
+            {/* Top Mixes */}
+            <div>
+              <TopMixes />
+            </div>
           </div>
 
-          {/* Most Liked */}
-          <div className="w-full">
-            <MostLikely />
-          </div>
+          <div className="flex flex-col space-y-4 w-full bg-dark-200 px-4 pb-4">
+            {/* Top Mixes */}
+            <div>
+              <TopMixes />
+            </div>
 
-          {/* Top Mixes */}
-          <div>
-            <TopMixes />
-          </div>
+            {/* Top Mixes */}
+            <div>
+              <TopMixes />
+            </div>
 
-          {/* Top Mixes */}
-          <div>
-            <TopMixes />
-          </div>
-
-          {/* Top Mixes */}
-          <div>
-            <TopMixes />
-          </div>
-
-          {/* Top Mixes */}
-          <div>
-            <TopMixes />
+            {/* Top Mixes */}
+            <div>
+              <TopMixes />
+            </div>
           </div>
         </div>
       </div>
